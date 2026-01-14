7926d27ce353bb07ad67797d761f49f0fc845c73
Brent Neal <bneal5@twc.com>
2026-01-14 14:02:53 -0500

    Comment out resource creation for demo; use existing AWS infra

--- modules/tf-module-example/main.tf
+++ modules/tf-module-example/main.tf
@@ -1,108 +1,130 @@
-########################################
-# Provider
-#######################################
+###########################################################
+# Provider configuration
+###########################################################
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
-#######################################
-# Networking (VPC, subnet, routing)
-#######################################
+###########################################################
+# Networking resources (DISABLED for demo)
+###########################################################
 
-resource "aws_vpc" "this" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# NOTE:
+# The VPC, subnet, and security group below were already created in AWS.
+# For this GitHub Actions demo, we comment them out so no new resources
+# are created or modified. The code is left here to demonstrate how the
+# module would normally provision networking.
 
-  tags = {
-    Name = "${var.ec2_instance_name}-vpc"
-  }
-}
+# resource "aws_vpc" "this" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+#
+#   tags = merge(
+#     {
+#       Name = "tf-module-example-vpc"
+#     },
+#     var.common_tags,
+#   )
+# }
+#
+# resource "aws_subnet" "public" {
+#   vpc_id                  = aws_vpc.this.id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#
+#   tags = merge(
+#     {
+#       Name = "tf-module-example-public-subnet"
+#     },
+#     var.common_tags,
+#   )
+# }
+#
+# resource "aws_security_group" "ec2" {
+#   name        = "tf-module-example-ec2-sg"
+#   description = "Security group for example EC2 instance"
+#   vpc_id      = aws_vpc.this.id
+#
+#   # Example ingress rule (e.g., SSH)
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   tags = merge(
+#     {
+#       Name = "tf-module-example-ec2-sg"
+#     },
+#     var.common_tags,
+#   )
+# }
 
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
+###########################################################
+# EC2 instance (DISABLED for demo)
+###########################################################
 
-  tags = {
-    Name = "${var.ec2_instance_name}-igw"
-  }
-}
+# NOTE:
+# The EC2 instance (base-image-builder / i-0985f0f8ced553d73) has already
+# been created in AWS. For this demo, we comment out the instance
+# resource so terraform apply is effectively a no-op.
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+# resource "aws_instance" "this" {
+#   ami           = var.ec2_ami_id
+#   instance_type = var.ec2_instance_type
+#
+#   # If we were creating networking here:
+#   # subnet_id              = aws_subnet.public.id
+#   # vpc_security_group_ids = [aws_security_group.ec2.id]
+#
+#   # For existing networking via variables, we would use:
+#   # subnet_id              = var.subnet_id
+#   # vpc_security_group_ids = var.security_group_ids
+#
+#   tags = merge(
+#     {
+#       Name = var.ec2_instance_name
+#     },
+#     var.common_tags,
+#   )
+# }
 
-  tags = {
-    Name = "${var.ec2_instance_name}-public-subnet"
-  }
-}
+###########################################################
+# Outputs (optional, kept for documentation)
+###########################################################
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
-  tags = {
-    Name = "${var.ec2_instance_name}-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-#######################################
-# Security group
-#######################################
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "${var.ec2_instance_name}-sg"
-  description = "Security group for EC2 instance"
-  vpc_id      = aws_vpc.this.id
-
-  # Allow SSH from anywhere (for testing only; tighten later)
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.ec2_instance_name}-sg"
-  }
-}
-
-#######################################
-# EC2 instance
-#######################################
-
-resource "aws_instance" "sample" {
-  ami                         = var.ec2_ami_id
-  instance_type               = var.ec2_instance_type
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
-
-  # Use the key pair you created in AWS (you can also make this a variable)
-  key_name = "tf-builder-key"
-
-  tags = {
-    Name = var.ec2_instance_name
-  }
-}
+# output "vpc_id" {
+#   description = "ID of the VPC created by this module"
+#   value       = aws_vpc.this.id
+# }
+#
+# output "public_subnet_id" {
+#   description = "ID of the public subnet"
+#   value       = aws_subnet.public.id
+# }
+#
+# output "ec2_instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.this.id
+# }
