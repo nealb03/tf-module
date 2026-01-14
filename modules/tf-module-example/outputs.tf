f74cc141f7bf0446bf17b37b3783154a7cfdf519
Brent Neal <bneal5@twc.com>
2026-01-14 14:16:16 -0500

    Align outputs.tf on my-branch with main

--- modules/tf-module-example/outputs.tf
+++ modules/tf-module-example/outputs.tf
@@ -1,14 +1,24 @@
-output "ec2_instance_id" {
-  value       = aws_instance.sample.id
-  description = "The AWS ID of the EC2 instance we created."
-}
+###########################################################
+# Outputs (DISABLED for demo)
+###########################################################
 
-output "ec2_public_ip" {
-  value       = aws_instance.sample.public_ip
-  description = "The public IP of the EC2 instance."
-}
+# NOTE:
+# These outputs referenced resources that we have commented out in main.tf
+# (aws_instance.sample, aws_vpc.this). For the GitHub Actions demo,
+# we disable the outputs so terraform validate/plan/apply succeed with no
+# managed resources. The code is left here for interview review.
 
-output "vpc_id" {
-  value       = aws_vpc.this.id
-  description = "The ID of the created VPC."
-}
+# output "ec2_instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.sample.id
+# }
+#
+# output "ec2_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.sample.public_ip
+# }
+#
+# output "vpc_id" {
+#   description = "ID of the VPC created by this module"
+#   value       = aws_vpc.this.id
+# }
